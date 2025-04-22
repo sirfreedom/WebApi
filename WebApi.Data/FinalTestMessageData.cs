9fa4c111afdc0f2779edad6d6ef833bf00feddc3
Alejandro Millan <alejandro.millan@GrupoCETA.com.ar>
2025-04-22 17:36:31 -0300

    se quitan metodos sin store
    se quitan metodos sin store

--- WebApi.Data/FinalTestMessageData.cs
+++ WebApi.Data/FinalTestMessageData.cs
@@ -24,40 +24,7 @@ namespace WebApi.Data
             return FinalTestMessage.ToList<FinalTestMessage>(ds.Tables[0]);
         }
 
-        public List<FinalTestMessage> List()
-        {
-            IRepository<FinalTestMessage> Serv;
-            Serv = new ContextSQL<FinalTestMessage>(_ConnectionString);
-            return Serv.List();
-        }
-
-        public List<dynamic> Find(FinalTestMessage finalTestMessage) 
-        {
-            IRepository<FinalTestMessage> Serv;
-            Serv = new ContextSQL<FinalTestMessage>(_ConnectionString);
-            return Serv.Find(finalTestMessage);
-        }
-
-        public void Update(FinalTestMessage finalTestMessage) 
-        {
-            IRepository<FinalTestMessage> Serv;
-            Serv = new ContextSQL<FinalTestMessage>(_ConnectionString);
-            Serv.Update(finalTestMessage);
-        }
-
-        public void Insert(FinalTestMessage finalTestMessage) 
-        {
-            IRepository<FinalTestMessage> Serv;
-            Serv = new ContextSQL<FinalTestMessage>(_ConnectionString);
-            Serv.Insert(finalTestMessage);
-        }
-
-        public void Delete(int Id)
-        {
-            IRepository<FinalTestMessage> Serv;
-            Serv = new ContextSQL<FinalTestMessage>(_ConnectionString);
-            Serv.Delete(Id);
-        }
+ 
 
         
 
