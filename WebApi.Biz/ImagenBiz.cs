cb46df79e493569d5a0422a22ec6143e1e2dfb49
sirfreedom <alemillan22@gmail.com>
2025-05-18 22:27:57 -0300

    .
    .

--- WebApi.Biz/ImagenBiz.cs
+++ WebApi.Biz/ImagenBiz.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using WebApi.Data;
 using WebApi.Entity;
+using System.Net;
+using System.Net.Mail;
 
 namespace WebApi.Biz
 {
@@ -29,7 +31,41 @@ namespace WebApi.Biz
         {
             ImagenTestData Serv = new ImagenTestData(_ConectionString);
             Serv.Delete(Id);
-        } 
+        }
+
+        public void SendMail(string from, string password, string to, string subject, string body, string[] archivosAdjuntos) 
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from);
+            mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true; // o false si es texto plano
+
+
+            //Attachment.
+
+            // Agregar archivos adjuntos
+            //if (archivosAdjuntos != null)
+            //{
+            //    foreach (string archivo in archivosAdjuntos)
+            //    {
+            //        if (System.IO.File.Exists(archivo))
+            //        {
+            //            Attachment adjunto = new Attachment(archivo);
+            //            mail.Attachments.Add(adjunto);
+            //        }
+            //    }
+            //}
+
+
+            //mail.Attachments.Add()
+            // Configurar cliente SMTP (ejemplo con Gmail)
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential(from, password);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
 
 
     }
