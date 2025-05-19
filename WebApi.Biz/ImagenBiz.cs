9e3d066598552ad9fdd52d17844799373da09f5e
Alejandro Millan <alejandro.millan@GrupoCETA.com.ar>
2025-05-19 16:03:46 -0300

    se elimina envio de correo
    se elimina envio de correo

--- WebApi.Biz/ImagenBiz.cs
+++ WebApi.Biz/ImagenBiz.cs
@@ -33,39 +33,7 @@ namespace WebApi.Biz
             Serv.Delete(Id);
         }
 
-        public void SendMail(string from, string password, string to, string subject, string body, string[] archivosAdjuntos) 
-        {
-            MailMessage mail = new MailMessage();
-            mail.From = new MailAddress(from);
-            mail.To.Add(to);
-            mail.Subject = subject;
-            mail.Body = body;
-            mail.IsBodyHtml = true; // o false si es texto plano
-
-
-            //Attachment.
-
-            // Agregar archivos adjuntos
-            //if (archivosAdjuntos != null)
-            //{
-            //    foreach (string archivo in archivosAdjuntos)
-            //    {
-            //        if (System.IO.File.Exists(archivo))
-            //        {
-            //            Attachment adjunto = new Attachment(archivo);
-            //            mail.Attachments.Add(adjunto);
-            //        }
-            //    }
-            //}
-
-
-            //mail.Attachments.Add()
-            // Configurar cliente SMTP (ejemplo con Gmail)
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new NetworkCredential(from, password);
-            smtp.EnableSsl = true;
-            smtp.Send(mail);
-        }
+   
 
 
     }
