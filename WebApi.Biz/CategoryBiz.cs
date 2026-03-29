f21e5d708860f04781f4107281b26eb3fc17e79c
sirfreedom <sirfreedom@users.noreply.github.com>
2026-03-29 11:33:53 -0300

    Se Agrega categoria
    Se Agrega categoria

---
+++ WebApi.Biz/CategoryBiz.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System;
+using WebApi.Entity;
+using WebApi.Data;
+using System.Threading.Tasks;
+
+namespace WebApi.Biz
+{
+    public class CategoryBiz
+    {
+        private readonly string _ConnectionString = string.Empty;
+
+        public CategoryBiz(string ConnectionString)
+        {
+            _ConnectionString = ConnectionString;
+        }
+
+        public async Task<List<Category>> Find(int IdAppConfig)
+        {
+            CategoryData oCategoryData = new(_ConnectionString);
+            List<Category> lCategory;
+            try
+            {
+                lCategory = await oCategoryData.Find(IdAppConfig);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return lCategory;
+        }
+
+
+    }
+}
