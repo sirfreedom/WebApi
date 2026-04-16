1513f13366ffdb5bbf7e008cfc80cd72f85090a7
sirfreedom <sirfreedom@users.noreply.github.com>
2026-04-16 00:40:15 -0300

    se Agrega el abm de product store
    se Agrega el abm de product store

---
+++ WebApi.Entity/ProductStore.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WebApi.Entity
+{
+	public class ProductStore : EntityBase
+	{
+		public string DescriptionText { get; set; }
+		public int IdCategory { get; set; }
+		public int IdUserStoreData { get; set; }
+		public bool isnew { get; set; } = false;
+		public decimal price { get; set; }
+		public int Quantity { get; set; }
+		public string YouTubeLink { get; set; }
+
+		public string imgtext1 { get; set; }
+        public string imgtext2 { get; set; }
+        public string imgtext3 { get; set; }
+        public string imgtext4 { get; set; }
+    }
+}
---
+++ WebApi/Model/ProductStoreFindModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebApi.Model
+{
+	public class ProductStoreFindModel
+	{
+		public string DescriptionText { get; set; }
+		public bool IsNew { get; set; }
+	}
+
+}
